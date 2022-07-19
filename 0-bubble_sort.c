10b06499ca72ba124cfcd6164b102bd21ddc046d
MelakuAlehegn <49185991+MelakuAlehegn@users.noreply.github.com>
2022-07-19 22:35:08 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,19 @@
 #include "sort.h"
 
 /**
-* bubble_sort - c function that uses uses the Buble sort method in an array
-* @array: array of integers to sort
-* @size: the size of the array to be sorted
-* Return: Sorted array
-*/
-
+ * bubble_sort - c function that uses uses the Buble sort method in an array
+ * @array: array of integers to sort
+ * @size: the size of the array to be sorted
+ * Return: Sorted array
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t n = size;
+size_t check = size;
 size_t i, temp = 0;
 
 if (array == NULL || size == 0)
-{
 return;
-}
-while (n > 0)
+while (check > 0)
 {
 for (i = 0; i < size - 1; i++)
 {
@@ -28,6 +25,6 @@ array[i] = temp;
 print_array(array, size);
 }
 }
-n--;
+check--;
 }
 }
